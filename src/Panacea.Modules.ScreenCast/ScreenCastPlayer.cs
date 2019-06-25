1b36b34ec027b49260bbe1891987ed91a0810f68
Kostas Aronis <k.aronis@dotbydot.gr>
2019-06-25 11:55:07 +0300

    cleanup and comments

--- src/Panacea.Modules.ScreenCast/ScreenCastPlayer.cs
+++ src/Panacea.Modules.ScreenCast/ScreenCastPlayer.cs
@@ -46,61 +46,20 @@ namespace Panacea.Modules.ScreenCast
             }
         }
 
-        private void OnMessageFromSlave(MediaPlayerMessage msg)
-        {
-            switch (msg.Action)
-            {
-                case "playing":
-                    Playing?.Invoke(this, null);
-                    break;
-                case "stopped":
-                    Stopped?.Invoke(this, null);
-                    break;
-                case "error":
-                    Error?.Invoke(this, null);
-                    break;
-                case "ended":
-                    Ended?.Invoke(this, null);
-                    break;
-                case "now-playing":
-                    NowPlaying?.Invoke(this, msg.Mrl);
-                    break;
-                case "position":
-                    PositionChanged?.Invoke(this, msg.Position);
-                    break;
-                case "paused":
-                    Paused?.Invoke(this, null);
-                    break;
-                case "opening":
-                    Opening?.Invoke(this, null);
-                    break;
-                default:
-                    break;
-            }
-        }
+
+        /* Messages from master to slave to ask for action or information
+         */
         private void OnMessageFromMaster(MediaPlayerMessage msg)
         {
             IMediaPlayerContainer container;
             IUiManager uiManager;
+            IAudioManager _audio;
             switch (msg.Action)
             {
                 case "subtitles-on":
                     try
                     {
-                        if (_core.TryGetMediaPlayerContainer(out container))
-                        {
-                            if (result.HasSubtitles)
-                            {
-                                result.SetSubtitles(true);
-                            }
-                            else
-                            {
-                                if (_core.TryGetUiManager(out uiManager))
-                                {
-                                    uiManager.Toast(new Translator("ScreenCast").Translate("Subtitles not found for this media"));
-                                }
-                            }
-                        }
+                        result?.SetSubtitles(true);
                     }
                     catch
                     {
@@ -113,11 +72,7 @@ namespace Panacea.Modules.ScreenCast
                 case "subtitles-off":
                     try
                     {
-
-                        if (_core.TryGetMediaPlayerContainer(out container))
-                        {
-                            result.SetSubtitles(false);
-                        }
+                        result?.SetSubtitles(false);
                     }
                     catch
                     {
@@ -134,7 +89,8 @@ namespace Panacea.Modules.ScreenCast
                         {
                             result = container.Play(new MediaRequest(new IptvMedia() { URL = msg.Mrl + " " + msg.Extras }));
                             AttachToMediaResponse(result);
-                        } else
+                        }
+                        else
                         {
                             result.Play();
                         }
@@ -167,21 +123,80 @@ namespace Panacea.Modules.ScreenCast
                 case "volume":
                     if (_core.TryGetMediaPlayerContainer(out container))
                     {
-                        if(_core.TryGetAudioManager(out IAudioManager _audio)){
-                            _audio.SpeakersVolume = (int)(msg.Volume*100f);
+                        if (_core.TryGetAudioManager(out _audio))
+                        {
+                            _audio.SpeakersVolume = (int)(msg.Volume * 100f);
                         }
                     }
                     break;
                 case "getvolume":
                     if (_core.TryGetMediaPlayerContainer(out container))
                     {
-                        //Task.Run(() => container.Send("mediaplayer", new MediaPlayerMessage { Action = "getvolume", Volume = Host.AudioManager.SpeakersVolume }));
+                        if (_core.TryGetAudioManager(out _audio))
+                        {
+                            Task.Run(() => BoundTerminal.Send("mediaplayer", new MediaPlayerMessage { Action = "getvolume", Volume = _audio.SpeakersVolume }));
+                        }
                     }
                     break;
                 default:
                     break;
             }
         }
+
+
+        /* Messages sent from slave to master to inform about the current state
+         */
+        private void OnMessageFromSlave(MediaPlayerMessage msg)
+        {
+            switch (msg.Action)
+            {
+                case "playing":
+                    Playing?.Invoke(this, null);
+                    break;
+                case "stopped":
+                    Stopped?.Invoke(this, null);
+                    break;
+                case "error":
+                    Error?.Invoke(this, null);
+                    break;
+                case "ended":
+                    Ended?.Invoke(this, null);
+                    break;
+                case "now-playing":
+                    NowPlaying?.Invoke(this, msg.Mrl);
+                    break;
+                case "position":
+                    PositionChanged?.Invoke(this, msg.Position);
+                    break;
+                case "paused":
+                    Paused?.Invoke(this, null);
+                    break;
+                case "opening":
+                    Opening?.Invoke(this, null);
+                    break;
+                case "seekable-changed":
+                    //TODO: Need to be able to send bool
+                    break;
+                case "pausable-changed":
+                    //TODO: Need to be able to send bool
+                    break;
+                case "has-subtitles-changed":
+                    //TODO: Need to be able to send bool
+                    break;
+                case "has-next-changed":
+                    //TODO: Need to be able to send bool
+                    break;
+                case "duration-changed":
+                    //TODO: Need to be able to send bool
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+        /* Slave attaches to media response's events and communicates every change to it's bound master.
+         */
         void AttachToMediaResponse(IMediaResponse result)
         {
             result.Playing += Result_Playing;
@@ -201,54 +216,44 @@ namespace Panacea.Modules.ScreenCast
         }
         private void Result_DurationChanged(object sender, TimeSpan e)
         {
-            throw new NotImplementedException();
+            Task.Run(() => BoundTerminal.Send("mediaplayer", new MediaPlayerMessage() { Action = "duration-changed" }));
         }
-
         private void Result_HasNextChanged(object sender, bool e)
         {
-            throw new NotImplementedException();
+            Task.Run(() => BoundTerminal.Send("mediaplayer", new MediaPlayerMessage() { Action = "has-next-changed" }));
         }
-
         private void Result_HasPreviousChanged(object sender, bool e)
         {
-            throw new NotImplementedException();
+            Task.Run(() => BoundTerminal.Send("mediaplayer", new MediaPlayerMessage() { Action = "has-previous-changed" }));
         }
-
         private void Result_HasSubtitlesChanged(object sender, bool e)
         {
-            throw new NotImplementedException();
+            Task.Run(() => BoundTerminal.Send("mediaplayer", new MediaPlayerMessage() { Action = "has-subtitles-changed" }));
         }
-
         private void Result_IsPausableChanged(object sender, bool e)
         {
-            throw new NotImplementedException();
+            Task.Run(() => BoundTerminal.Send("mediaplayer", new MediaPlayerMessage() { Action = "pausable-changed" }));
         }
-
         private void Result_IsSeekableChanged(object sender, bool e)
         {
             Task.Run(() => BoundTerminal.Send("mediaplayer", new MediaPlayerMessage() { Action = "seekable-changed"}));
         }
-
         private void Result_Opening(object sender, EventArgs e)
         {
             Task.Run(() => BoundTerminal.Send("mediaplayer", new MediaPlayerMessage() { Action = "opening" }));
         }
-
         private void Result_Paused(object sender, EventArgs e)
         {
             Task.Run(() => BoundTerminal.Send("mediaplayer", new MediaPlayerMessage() { Action = "paused"}));
         }
-
         private void Result_PositionChanged(object sender, float e)
         {
             Task.Run(() => BoundTerminal.Send("mediaplayer", new MediaPlayerMessage() { Action = "position", Position=e }));
         }
-
         private void Result_NowPlaying(object sender, string e)
         {
             Task.Run(() => BoundTerminal.Send("mediaplayer", new MediaPlayerMessage() { Action = "now-playing", Mrl = e }));
         }
-
         private void Result_Error(object sender, Exception e)
         {
             Task.Run(() => BoundTerminal.Send("mediaplayer", new MediaPlayerMessage() { Action = "error" }));
@@ -266,6 +271,9 @@ namespace Panacea.Modules.ScreenCast
             Task.Run(() => BoundTerminal.Send("mediaplayer", new MediaPlayerMessage() { Action = "playing" }));
         }
 
+        
+        /* Events invoked by master to to be used by other plugins in the master terminal
+         */
         public event EventHandler<bool> IsSeekableChanged;
         public event EventHandler<float> PositionChanged;
         public event EventHandler<bool> HasNextChanged;
@@ -281,33 +289,23 @@ namespace Panacea.Modules.ScreenCast
         public event EventHandler Ended;
         public event EventHandler<Exception> Error;
 
-        public bool IsSeekable { get => false; }
 
+        /* Properties to keep inner state about the media now playing.
+         */
+        public bool IsSeekable { get => false; }
         private float _position;
         public float Position { get => _position; set => _position = value; }
-
         private bool _isPlaying;
         public bool IsPlaying { get => _isPlaying; }
-
         public bool HasNext { get => false; }
-
         public bool HasPrevious { get => false; }
-        public bool IsPausable
-        {
-            get
-            {
-                if (_core.TryGetMediaPlayerContainer(out IMediaPlayerContainer container))
-                {
-                    return result.IsPausable;
-                }
-                return false;
-            }
-        }
+        public bool IsPausable { get; }
+        public TimeSpan Duration { get; }
+        public bool HasSubtitles { get; }
 
-        public TimeSpan Duration => throw new NotImplementedException();
 
-        public bool HasSubtitles => throw new NotImplementedException();
-
+        /* Public methods to be called on master from other plugins
+         */
         public void Next()
         {
             Task.Run(() => BoundTerminal.Send("mediaplayer", new MediaPlayerMessage { Action = "next"}));
@@ -318,24 +316,24 @@ namespace Panacea.Modules.ScreenCast
         }
         public void Pause()
         {
-            BoundTerminal.Send("mediaplayer", new MediaPlayerMessage() { Action = "pause" });
+            Task.Run(() => BoundTerminal.Send("mediaplayer", new MediaPlayerMessage() { Action = "pause" }));
         }
         public void Play(string url, MediaItem media)
         {
-            BoundTerminal.Send("mediaplayer", new MediaPlayerMessage() { Action = "play", Mrl = media.GetMRL(), Extras = media.GetExtras() });
+            Task.Run(() => BoundTerminal.Send("mediaplayer", new MediaPlayerMessage() { Action = "play", Mrl = media.GetMRL(), Extras = media.GetExtras() }));
         }
         public void Play()
         {
-            BoundTerminal.Send("mediaplayer", new MediaPlayerMessage() { Action = "play" });
+            Task.Run(() => BoundTerminal.Send("mediaplayer", new MediaPlayerMessage() { Action = "play" }));
         }
         public void SetSubtitles(bool on)
         {
             if (on)
             {
-                BoundTerminal.Send("mediaplayer", new MediaPlayerMessage() { Action = "subtitles-on" });
+                Task.Run(() => BoundTerminal.Send("mediaplayer", new MediaPlayerMessage() { Action = "subtitles-on" }));
             } else
             {
-                BoundTerminal.Send("mediaplayer", new MediaPlayerMessage() { Action = "subtitles-off" });
+                Task.Run(() => BoundTerminal.Send("mediaplayer", new MediaPlayerMessage() { Action = "subtitles-off" }));
             }
         }
         public void SetVolume(int value)
@@ -346,6 +344,9 @@ namespace Panacea.Modules.ScreenCast
         {
             Task.Run(() => BoundTerminal.Send("mediaplayer", new MediaPlayerMessage { Action = "stop" }));
         }
+
+        /* Currently unused helper functions
+         */
         private bool roleIs(TerminalRelation role)
         {
             return BoundTerminal.Relation == role;
